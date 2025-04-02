f2f3a32fee5ec173557324a1dc5a0e1f050b0b07
hey_zeus <valdesj419@gmail.com>
2025-04-01 21:30:57 -0700

    Initialize CleverTap SDK and verify active user appears in CleverTap dashboard.

--- app/src/main/java/com/heyzeusv/clevertapassessment/MainActivity.kt
+++ app/src/main/java/com/heyzeusv/clevertapassessment/MainActivity.kt
@@ -11,11 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.clevertap.android.sdk.CleverTapAPI
 import com.heyzeusv.clevertapassessment.ui.theme.CleverTapAssessmentTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		val cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)
+
 		enableEdgeToEdge()
 		setContent {
 			CleverTapAssessmentTheme {
