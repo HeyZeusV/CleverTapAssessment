ab67c2d49b6bfd5429ce737e4efefa1846c48df7
hey_zeus <valdesj419@gmail.com>
2025-04-01 23:14:52 -0700

    Add 2 functions: One creates a dummy account with static basic information and includes string array MyStuff; the other updates dummy account's MyStuff with random strings. Update UI with 2 buttons that run one function each.

--- app/src/main/java/com/heyzeusv/clevertapassessment/MainActivity.kt
+++ app/src/main/java/com/heyzeusv/clevertapassessment/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.heyzeusv.clevertapassessment.ui.theme.CleverTapAssessmentTheme
 import org.koin.androidx.compose.koinViewModel
 
@@ -29,9 +34,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(mainVM: MainViewModel = koinViewModel()) {
 	Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-		Text(
-			text = "AccountId: ${mainVM.getCleverTapAccountId()}",
-			modifier = Modifier.padding(innerPadding)
-		)
+		Column(
+			modifier = Modifier
+				.padding(innerPadding)
+				.fillMaxSize(),
+			verticalArrangement = Arrangement.spacedBy(8.dp),
+			horizontalAlignment = Alignment.CenterHorizontally,
+		) {
+			Text(text = "AccountId: ${mainVM.getCleverTapAccountId()}")
+			Button(onClick = { mainVM.createDummyAccount() }) {
+				Text("Create/reset dummy account")
+			}
+			Button(onClick = { mainVM.updateDummyAccountMyStuff() }) {
+				Text("Update dummy stuff")
+			}
+		}
 	}
 }
--- app/src/main/java/com/heyzeusv/clevertapassessment/MainViewModel.kt
+++ app/src/main/java/com/heyzeusv/clevertapassessment/MainViewModel.kt
@@ -1,9 +1,47 @@
 package com.heyzeusv.clevertapassessment
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.clevertap.android.sdk.CleverTapAPI
+import java.util.Date
+import kotlin.random.Random
 
 class MainViewModel(private val cleverTapAPI: CleverTapAPI) : ViewModel() {
 
 	fun getCleverTapAccountId(): String = cleverTapAPI.accountId
+
+	fun createDummyAccount() {
+		val profileUpdate = HashMap<String, Any>()
+		profileUpdate["Name"] = "Test Dummy" // String
+		profileUpdate["Identity"] = 1234 // String or number
+		profileUpdate["Email"] = "test@gmail.com" // Email address of the user
+		profileUpdate["Phone"] = "+14155551234" // Phone (with the country code, starting with +)
+		profileUpdate["Gender"] = "M" // Can be either M or F
+		profileUpdate["DOB"] = Date() // Date of Birth. Set the Date object to the appropriate value first
+
+
+		val otherStuff = arrayOf("Random", "Stuff")
+		profileUpdate["MyStuff"] = otherStuff //String Array
+		cleverTapAPI.onUserLogin(profileUpdate)
+	}
+
+	fun updateDummyAccountMyStuff() {
+		val profileUpdate = HashMap<String, Any>()
+
+		val stuff = mutableListOf<String>()
+		repeat(Random.nextInt(0, 5)) {
+			stuff.add(randomString())
+		}
+		Log.i("CleverTapAssessment", "Updated MyStuff: $stuff")
+		profileUpdate["MyStuff"] = stuff
+		cleverTapAPI.pushProfile(profileUpdate)
+	}
+
+	private fun randomString(): String {
+		val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+		return (1..10)
+			.map { Random.nextInt(0, charPool.size).let { charPool[it] } }
+			.joinToString("")
+	}
 }
